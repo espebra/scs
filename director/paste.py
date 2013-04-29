ab5b2b9cc141cee87bd42ccd2384c9429d4f0445
Espen Braastad <espebra@ifi.uio.no>
2013-04-29 13:19:20 +0200

    Initial commit. Rewritten from scratch in Lua!

--- director/paste.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python
-
-import paste
-from paste import httpserver
-from director import app
-
-paste.httpserver.serve(app, host=app.config['HOST'], port=app.config['PORT'])
-
--- storage/index.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python
-from wsgiref.handlers import CGIHandler
-from storage import app
-
-CGIHandler().run(app)
