dac20483413eb2c164d93161279014477f82a673
Espen Braastad <espebra@ifi.uio.no>
2013-10-24 22:58:52 +0200

    Added full replication. This feature has a known flaw as it stops the world when being executed. The feature should be extracted to a command line script.

--- scs/common.lua
+++ scs/common.lua
@@ -677,5 +677,54 @@ function M.is_checksum_valid(bucket, object, version, md5)
     valid = true
     return valid
 end
+
+function M.full_replication()
+    local path = M.get_storage_directory()
+    local entry, versions, popen = nil, {}, io.popen
+    for entry in popen('find ' .. path .. ' -type f -printf "%T@\t%s\t%f\t%h\n" | sort -nr'):lines() do
+        local m, err = ngx.re.match(entry, "^([0-9]+)[^\t]+\t([0-9]+)\t([0-9]+)-([a-f0-9]+).([a-z]+)\t" .. path .. "/([^/]+).*/([^/]+)$","j")
+        if m then
+            if #m == 7 then
+                local mtime = tonumber(m[1])
+                local size = tonumber(m[2])
+                local version = tonumber(m[3])
+                local md5 = m[4]
+                local filetype = m[5]
+                local bucket = m[6]
+                local object_base64 = m[7]
+                local object = ngx.decode_base64(object_base64)
+                local filename = version .. "-" .. md5 .. "." .. filetype
+
+                -- ngx.log(ngx.ERR,"mtime: " .. m[1] .. ", size: " .. m[2] .. ", version: " .. m[3] .. ", md5: " .. m[4] .. ", type: " .. m[5] .. ", bucket: " .. m[6])
+
+                if filetype == "data" then
+                    local valid = M.is_checksum_valid(bucket, object, version, md5)
+                    if valid then
+                        -- Replicate to other hosts.
+                        local sites = M.get_object_replica_sites(bucket, object)
+                        local hosts = M.get_replica_hosts(bucket, object, sites)
+                        for _,host in pairs(hosts) do
+                            if M.get_host_status(host) then
+                                local res = M.sync_object(host, bucket, object, filename)
+                                if res then
+                                    ngx.log(ngx.INFO,"Object " .. bucket .. "/" .. object .. " version " .. version .. " was replicated to " .. host)
+                                else
+                                    ngx.log(ngx.ERR,"Object " .. bucket .. "/" .. object .. " version " .. version .. " was NOT replicated to " .. host)
+                                end
+                            else
+                                ngx.log(ngx.ERR,host .. " is down. Unable to replicate.")
+                            end
+                        end
+                    else
+                        ngx.log(ngx.ERR,"Object " .. bucket .. "/" .. object .. " version " .. version .. " is corrupt")
+                        -- M.quarantine(bucket, object, version, md5)
+                    end
+                -- elseif filetype == "ts" then
+                    -- Remove old versions if tombstone exists.
+                end
+            end
+        end
+    end
+end
     
 return M
